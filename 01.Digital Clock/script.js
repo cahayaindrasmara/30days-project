5b6ed439deebaa9493de29071a68d9fdcf41f6af
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-05-20 21:30:02 +0700

    add format AM/PM, tanggal hari ini, ganti tema

--- 01.Digital Clock/script.js
+++ 01.Digital Clock/script.js
@@ -1,10 +1,19 @@
 function updateClock() {
     const now = new Date();
     // new Date() berfungsi untuk mengambil waktu sekarang
+    // jika di log akan menampilkan tanggal hari ini
 
-    let hours = now.getHours().toString().padStart(2, "0");
+    let hours = now.getHours();
     let minutes = now.getMinutes().toString().padStart(2, "0");
     let seconds = now.getSeconds().toString().padStart(2, "0");
+
+    //format AM/PM
+    const ampm = hours >= 12 ? "PM" : "AM"
+    hours = hours % 12 || 12; //convert ke 12-jam dan hindari 0
+    /*
+    jika hours = 12 , maka 12 % 12 === 0
+    maka hours = 0 || 12, maka logika or akan memilih 12, karna o itu falsy dan 12 itu true karena ada value
+    */
 
     /*
     pasStart() digunakan untuk menambahkan karakter di depan string agar mencapai panjang tertentu
@@ -14,9 +23,33 @@ function updateClock() {
     padString: karakter pengisi (defaultnya spasi jika tidak diisi)
     */
 
-    const currentTime = `${hours}:${minutes}:${seconds}`
+    const timeString = `${hours.toString().padStart(2, "0")}:${minutes}:${seconds} ${ampm}`
 
-    document.getElementById("clock").textContent = currentTime;
+    document.getElementById("clock").textContent = timeString;
+
+    //tampilkan tanggal
+    const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" }
+    const dateString = now.toLocaleDateString("id-ID", options);
+
+    /*
+    toLocaleDateString("id-ID", options)
+    fungsi ini mengubah date (objek waktu) menjadi string tanggal yang diformat sesuai lokal tertentu
+
+    id-ID berarti bahasa dan format indonesia
+
+    locales: (opsional) kode lokal, misalnya "id-ID", "en-US"
+    options: (opsional) object berisi pengaturan format (seperti weekday, year, month, day), wajib berisi object
+    */
+    document.getElementById("date").textContent = dateString;
+
+    //ganti tema
+    const container = document.getElementById("container")
+    if (now.getHours() >= 6 && now.getHours() < 18) {
+        container.className = "clock-container day"
+        //digunakan untuk mengatur ulang seluruh class dari element, ini bisa : menimpa semua kelas yang ada atau digunakan untuk membaca nama class
+    } else {
+        container.className = "clock-container night"
+    }
 }
 
 setInterval(updateClock, 1000);
